38a13e68ec8def0b208a36706ce0e9a89a583e14
MonsiuerM <syedmurtazas19@gmail.com>
2022-07-27 19:56:50 +0500

    End of Section # 12

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -21,9 +21,12 @@ namespace API.Helpers
                         .ForMember(dest => dest.Age,
                         opt => opt.MapFrom(
                             src => src.DateOfBirth.CalculateAge()));
+            
             CreateMap<Photo, PhotoDto>();
 
             CreateMap<MemberUpdateDto, AppUser>();
+
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
